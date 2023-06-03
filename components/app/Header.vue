5d7fe7816ebd1880e429bbe19954949b6a0eeb95
Matteo De Micheli <matteodem@protonmail.com>
2023-06-03 14:46:05 +0200

    Adjust nav header colors

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="bg-sky-800 text-slate-200 py-4 px-3 font-bold">
+  <div class="bg-sky-600 text-slate-200 py-4 px-3 font-bold">
     <div class="flex">
       <div class="flex-1" :class="[
-        store.currentNavigationItem === 'index' ? 'text-white' : 'text-zinc-300'
+        store.currentNavigationItem === 'index' ? 'text-white' : 'text-gray-200'
       ]">
         <NuxtLink to="/">
           <MusicalNoteIcon class="h-5 w-5 inline-block align-middle mr-3" />
@@ -11,14 +11,14 @@
 
         <NuxtLink to="/upload/sound" 
         class="ml-8 mr-3 " :class="[
-          store.currentNavigationItem === 'upload' ? 'text-white' : 'text-zinc-300'
+          store.currentNavigationItem === 'upload' ? 'text-white' : 'text-gray-200'
         ]">
           <ArrowUpTrayIcon class="h-4 w-4 inline-block align-middle mr-3" />
           <span class="align-middle">Upload</span>
         </NuxtLink>
       </div>
       <div class="flex-initial" :class="[
-          store.currentNavigationItem === 'own-profile' ? 'text-white' : 'text-zinc-300'
+          store.currentNavigationItem === 'own-profile' ? 'text-white' : 'text-gray-200'
         ]">
         <NuxtLink to="/you/profile">
           <UserCircleIcon class="h-7 w-7"  />
