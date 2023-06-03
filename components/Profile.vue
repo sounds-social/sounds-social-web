65f12926c2763d7ba52320eee6d6c856680c136f
Matteo De Micheli <matteodem@protonmail.com>
2023-06-03 16:49:52 +0200

    Display last 4 likes in profile

--- components/Profile.vue
+++ components/Profile.vue
@@ -7,10 +7,38 @@
       Profile not found.
     </div>
 
-    <div v-if="sounds.length > 0">
-      <h2 class="text-2xl font-bold text-neutral-400 my-4">Recent</h2>
-      <SoundListPure :sounds="sounds" />
-  </div>
+    <div class="flex">
+      <div class="flex-1">
+        <div v-if="sounds.length > 0">
+          <h2 class="text-2xl font-bold text-neutral-400 my-4">Recent</h2>
+          <SoundListPure :sounds="sounds" />
+        </div>
+        <div v-else>
+          No sounds found.
+        </div>
+      </div>
+      <div class="flex-none w-60 pl-5">
+        <h2 class="font-bold text-lg mb-4">
+          <NuxtLink :to="`/likes/${slug}`">Likes</NuxtLink> 
+        </h2>
+
+        <div>
+          <div v-for="like in likes" :key="like.sound.id" class="inline-block">
+            <NuxtLink :to="`/sound/${like.sound.slug}`" class="w-24 h-24 mr-2 rounded inline-block cursor-pointer" :class="[
+                like.sound.coverUri ? 'bg-contain' : 'bg-gradient-to-r from-cyan-500 to-blue-500',
+              ]"
+              :style="`${like.sound.coverUri ? `background-image: url(${like.sound.coverUri})` : ''}`"
+            >
+            </NuxtLink>
+          </div>
+        </div>
+
+        <NuxtLink 
+          :to="`/likes/${slug}`"
+          class="text-gray-600 mt-3 ml-1"
+          v-if="likesLongerThanFour">See all likes</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
@@ -30,11 +58,20 @@ const store = useSoundStore();
 const query = gql`
   query SoundsForUser($slug: String!) {
     user(slug: $slug) {
+      likes {
+        sound {
+          coverUri
+          slug
+          id
+        }
+      }
+
       sounds {
         id
         title
         slug
         uri
+        coverUri
         owner {
           id
           slug
@@ -60,4 +97,11 @@ const { data, error } = await useAsyncQuery(query, {
 await store.loadSounds(data.value?.user?.sounds);
 
 const sounds = data.value?.user?.sounds;
+
+const likesLength = data.value?.user?.likes.length;
+
+const likesLongerThanFour = likesLength > 4;
+
+const likes = data.value?.user?.likes.slice(0, 4);
+
 </script>
